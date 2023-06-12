43600499a14e2d7c8cf452bd69946a07e4b9120e
Sedvin1903 <frsedric2003@gmail.com>
2023-06-12 21:04:23 +0530

    FINAL22222

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,38 @@ import Login from "./components/Login"
 import Signup from "./components/Signup"
 import Landing from "./components/Landing"
 import NotFound from "./components/NotFound"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+//import { useState } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+        <Route path="/" element={<Landing/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/*" element={<NotFound/>}/>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+/*
+// import './App.css'
+import Home from "./components/Home"
+import Login from "./components/Login"
+import Signup from "./components/Signup"
+import Landing from "./components/Landing"
+
 import  { createBrowserRouter, RouterProvider } from "react-router-dom";
 //import { useState } from 'react';
 
@@ -35,7 +67,7 @@ const router = createBrowserRouter([
     element: <Home/>, 
   },
   {
-    path: "/login", 
+    path: "./login", 
     element: <Login/>, 
   }, { path: "/*", element: <NotFound />, },
 
@@ -49,3 +81,4 @@ const router = createBrowserRouter([
 
 export default App;
 
+*/
