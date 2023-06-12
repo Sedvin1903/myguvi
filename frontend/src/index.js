24406c53c487e9a2a12ab2100df83a1496c4252b
Sedvin1903 <frsedric2003@gmail.com>
2023-06-12 20:41:52 +0530

    FINAL2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AuthContexProvider } from "./context/AuthContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthContexProvider>
+      <App />
+    </AuthContexProvider>
   </React.StrictMode>
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
