9371abb3b011c4e7f8eea1015085aa54cd43c93e
Sedvin1903 <frsedric2003@gmail.com>
2023-06-13 13:06:07 +0530

    FINAL

--- backend/router/router.js
+++ backend/router/router.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../Controller/controller");
 
-
-
 //=======================> User <====================
 
 router.post("/api/user/signup", userController.register);
